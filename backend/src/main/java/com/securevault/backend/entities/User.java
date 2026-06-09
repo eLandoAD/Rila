8d93b113d1bf1489b00f1d088315db7756b0c233
alessio.ferrari <alessio.ferrari@marconirovereto.it>
2026-06-09 13:35:57 +0300

    Feat: add User entity with fields

---
+++ backend/src/main/java/com/securevault/backend/entities/User.java
@@ -0,0 +1,60 @@
+package com.securevault.backend.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    // id
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // username
+    @Column(nullable = false, unique = true, length = 50)
+    private String username;
+
+    // email
+    @Column(nullable = false, unique = true, length = 100)
+    private String email;
+
+    // password hashata
+    @Column(nullable = false, length = 255)
+    private String password;
+
+    // roles
+    @Column(nullable = false)
+    private String roles = "ROLE_USER";
+    // Esempi validi:
+    // "ROLE_USER"
+    // "ROLE_USER,ROLE_ADMIN"
+    // "ROLE_USER,ROLE_TESTER"
+    // "ROLE_USER,ROLE_ADMIN,ROLE_TESTER"
+
+
+    // per disabilitare un utente
+    @Column(nullable = false)
+    private Boolean enabled = true;
+
+    // tempo creazione
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private Long createdAt = System.currentTimeMillis();
+
+    // tempo update
+    @Column(name = "updated_at")
+    private Long updatedAt;
+
+    // sempre update
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = System.currentTimeMillis();
+    }
+
+
+}
