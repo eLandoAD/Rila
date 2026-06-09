7ac6e8927a9b70273ac930dfd62555665283a4c4
Gecky2102 <giacomomasiero.08@gmail.com>
2026-06-09 13:42:47 +0300

    fix(routes): remove wildcard redirect and clean up route definitions

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -13,13 +13,10 @@ export const routes: Routes = [
   { path: 'verify-email', component: VerifyEmail },
   { path: 'forgot-password', component: ForgotPassword },
   { path: 'reset-password', component: ResetPassword },
-
-  { path: '**', redirectTo: 'login' }
-    // Add your routes here
-    {path: '', loadComponent: () => import('./landing-page/landing-page').then(m => m.LandingPage)},
-    {path: 'about', loadComponent: () => import('./about/about').then(m => m.About)},
-    {path: 'filemanager/dashboard', loadComponent: () => import('./filemanager/dashboard/dashboard').then(m => m.Dashboard)},
-    {path: 'filemanager/files', loadComponent: () => import('./filemanager/files/files').then(m => m.Files)},
-    {path: 'filemanager/uploads', loadComponent: () => import('./filemanager/upload/upload').then(m => m.Upload)},
-    {path: 'filemanager/shared', loadComponent: () => import('./filemanager/shared/shared').then(m => m.Shared)},
+  { path: '', loadComponent: () => import('./landing-page/landing-page').then(m => m.LandingPage)},
+  { path: 'about', loadComponent: () => import('./about/about').then(m => m.About)},
+  { path: 'filemanager/dashboard', loadComponent: () => import('./filemanager/dashboard/dashboard').then(m => m.Dashboard)},
+  { path: 'filemanager/files', loadComponent: () => import('./filemanager/files/files').then(m => m.Files)},
+  { path: 'filemanager/uploads', loadComponent: () => import('./filemanager/upload/upload').then(m => m.Upload)},
+  { path: 'filemanager/shared', loadComponent: () => import('./filemanager/shared/shared').then(m => m.Shared)},
 ];
