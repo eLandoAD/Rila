35cc08fdccef28f15acb4f14b5501c592202967f
Gecky2102 <giacomomasiero.08@gmail.com>
2026-06-09 10:43:20 +0300

    feat(landing-page): replace placeholder with hero section and description fix(footer): remove fixed positioning for better layout refactor(app): remove unnecessary HTML tag from app structure

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,5 +2,5 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
     // Add your routes here
-    
+    {path: '', loadComponent: () => import('./landing-page/landing-page').then(m => m.LandingPage)},
 ];
