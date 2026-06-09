6e499db779c84927375cf86567715bb7fa48acaf
Gecky2102 <giacomomasiero.08@gmail.com>
2026-06-09 13:27:11 +0300

    feat(filemanager): add upload component with side navbar layout

---
+++ frontend/src/app/filemanager/upload/upload.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { SideNavbar } from '../side-navbar/side-navbar';
+
+@Component({
+  selector: 'app-upload',
+  imports: [SideNavbar],
+  templateUrl: './upload.html',
+  styleUrl: './upload.css',
+})
+export class Upload {}
