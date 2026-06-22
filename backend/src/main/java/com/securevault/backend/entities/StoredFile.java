6bcd6b88b698f67bcf6878cc339b27da9e2e898e
alessio.ferrari <alessio.ferrari@marconirovereto.it>
2026-06-22 09:14:19 +0300

    Feat: adjust imports

--- backend/src/main/java/com/securevault/backend/entities/StoredFile.java
+++ backend/src/main/java/com/securevault/backend/entities/StoredFile.java
@@ -1,12 +1,20 @@
 package com.securevault.backend.entities;
 
 
-import jakarta.persistence.*;
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.util.UUID;
 
 @Entity
 @Table(name = "stored_files")
